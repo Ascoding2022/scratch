4a35a23a83e8fbac0bb9ec0de04c6a4a393be462
Ascoding2022 <119110065+Ascoding2022@users.noreply.github.com>
2023-04-29 17:41:05 +0000

    Pending changes exported from your codespace

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -2,7 +2,7 @@ const BlockType = require('../../extension-support/block-type');
 const ArgumentType = require('../../extension-support/argument-type');
 const TargetType = require('../../extension-support/target-type');
 
-class Scratch3YourExtension {
+class Cloud {
 
     constructor (runtime) {
         // put any setup for your extension here
@@ -13,88 +13,93 @@ class Scratch3YourExtension {
      */
     getInfo () {
         return {
-            // unique ID for your extension
-            id: 'yourScratchExtension',
-
-            // name that will be displayed in the Scratch UI
-            name: 'Demo',
-
-            // colours to use for your extension blocks
-            color1: '#000099',
-            color2: '#660066',
-
-            // icons to display
-            blockIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
-            menuIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
-
-            // your Scratch blocks
-            blocks: [
-                {
-                    // name of the function where your block code lives
-                    opcode: 'myFirstBlock',
-
-                    // type of block - choose from:
-                    //   BlockType.REPORTER - returns a value, like "direction"
-                    //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
-                    //   BlockType.COMMAND - a normal command block, like "move {} steps"
-                    //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
-                    blockType: BlockType.REPORTER,
-
-                    // label to display on the block
-                    text: 'My first block [MY_NUMBER] and [MY_STRING]',
-
-                    // true if this block should end a stack
-                    terminal: false,
-
-                    // where this block should be available for code - choose from:
-                    //   TargetType.SPRITE - for code in sprites
-                    //   TargetType.STAGE  - for code on the stage / backdrop
-                    // remove one of these if this block doesn't apply to both
-                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
-
-                    // arguments used in the block
-                    arguments: {
-                        MY_NUMBER: {
-                            // default value before the user sets something
-                            defaultValue: 123,
-
-                            // type/shape of the parameter - choose from:
-                            //     ArgumentType.ANGLE - numeric value with an angle picker
-                            //     ArgumentType.BOOLEAN - true/false value
-                            //     ArgumentType.COLOR - numeric value with a colour picker
-                            //     ArgumentType.NUMBER - numeric value
-                            //     ArgumentType.STRING - text value
-                            //     ArgumentType.NOTE - midi music value with a piano picker
-                            type: ArgumentType.NUMBER
-                        },
-                        MY_STRING: {
-                            // default value before the user sets something
-                            defaultValue: 'hello',
-
-                            // type/shape of the parameter - choose from:
-                            //     ArgumentType.ANGLE - numeric value with an angle picker
-                            //     ArgumentType.BOOLEAN - true/false value
-                            //     ArgumentType.COLOR - numeric value with a colour picker
-                            //     ArgumentType.NUMBER - numeric value
-                            //     ArgumentType.STRING - text value
-                            //     ArgumentType.NOTE - midi music value with a piano picker
-                            type: ArgumentType.STRING
-                        }
-                    }
+          // unique ID for your extension
+          id: 'cloud',
+      
+          // name displayed in the Scratch UI
+          name: 'Cloud',
+      
+          // colours to use for your extension blocks
+          color1: '#000099',
+          color2: '#660066',
+      
+          // your Scratch blocks
+          blocks: [
+            {
+              // function where your code logic lives
+              opcode: 'writeToCloud',
+      
+              // type of block
+              blockType: BlockType.COMMAND,
+      
+              // label to display on the block
+              text: 'Write [text] To Cloud On Line [line]',
+      
+              // arguments used in the block
+              arguments: {
+                text: {
+                  defaultValue: "Hello, world!",
+      
+                  // type/shape of the parameter
+                  type: ArgumentType.STRING
+                },
+                line: {
+                  defaultValue: "",
+      
+                  // type/shape of the parameter
+                  type: ArgumentType.NUMBER
                 }
-            ]
-        };
-    }
+              }
+            },
+            {
+              // function where your code logic lives
+              opcode: 'fetchCloudData',
+      
+              // type of block
+              blockType: BlockType.REPORTER,
+      
+              // label to display on the block
+              text: 'Fetch Cloud Data From Line [line]',
 
+              // arguments used in the block
+              arguments: {
+                line: {
+                  defaultValue: "",
+      
+                  // type/shape of the parameter
+                  type: ArgumentType.NUMBER
+                }
+              }
+            },
+            {
+              // function where your code logic lives
+              opcode: 'cloudLines',
+      
+              // type of block
+              blockType: BlockType.REPORTER,
+      
+              // label to display on the block
+              text: 'Cloud Lines',
+            },
+          ]
+        };
+      }
 
     /**
      * implementation of the block with the opcode that matches this name
      *  this will be called when the block is used
      */
-    myFirstBlock ({ MY_NUMBER, MY_STRING }) {
-        // example implementation to return a string
-        return MY_STRING + ' : doubled would be ' + (MY_NUMBER * 2);
-    }
+writeToCloud ({ }) {
+  return;
 }
 
-module.exports = Scratch3YourExtension;
+fetchCloudData ({ }) {
+  return;
+}
+
+cloudLines ({ }) {
+  return;
+}
+}
+
+module.exports = Cloud;
